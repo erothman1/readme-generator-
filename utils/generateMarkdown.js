5e3b1c29a5fc842126baa49a7f87b30fc5cf8eaa
Elva Rothman <erothman@tulane.edu>
2023-04-11 23:10:50 -0400

    wrote renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "MIT License":
+      return `![MIT](https://img.shields.io/badge/MIT-MIT%20License-ff69b4)`
+    case "Creative Commons Zero v1.0 Universal":
+      return `![Creative Commons](https://img.shields.io/badge/Creative%20Commons-Creative%20Commons%20Zero%20v1.0%20Universal-blueviolet)`
+    case "GNU General Public License v3.0":
+      return `![Creative Commons](https://img.shields.io/badge/GNU-General%20Public%20License%20v3.0-brightgreen)`
+    case "Apache License 2.0":
+      return `![Creative Commons](https://img.shields.io/badge/Apache-Apache%20License%202.0-9cf)`
+    default:
+      console.log("uh oh")
+      break
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +26,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} 
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
   
   ## Description 
   ${data.description}
@@ -33,6 +47,7 @@ function generateMarkdown(data) {
   ${data.credits}
 
   ##License
+  This project utilizes the ${data.license}
 
   ##How to Contribute 
   ${data.contribution}
